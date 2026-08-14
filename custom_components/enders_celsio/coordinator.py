7e7e013bab6b8c36d13f8914c64a223e2fe356b3
NikeRD96 <80649788+NikeRD96@users.noreply.github.com>
2026-08-14 16:10:42 +0200

    Add loading of last known Bluetooth information

--- custom_components/enders_celsio/coordinator.py
+++ custom_components/enders_celsio/coordinator.py
@@ -7,6 +7,7 @@ from typing import Any
 from homeassistant.components.bluetooth import (
     BluetoothScanningMode,
     BluetoothServiceInfoBleak,
+    async_last_service_info,
 )
 from homeassistant.components.bluetooth.passive_update_coordinator import (
     PassiveBluetoothDataUpdateCoordinator,
@@ -45,6 +46,14 @@ class EndersCelsioCoordinator(PassiveBluetoothDataUpdateCoordinator):
         self.address = address
         self.device_data: EndersCelsioData | None = None
 
+        # Letzte bekannte Bluetooth-Informationen sofort laden
+        last_info = async_last_service_info(hass, address, connectable=False)
+        if last_info:
+            initial_data = parse_service_info(last_info)
+            if initial_data:
+                self.device_data = initial_data
+                self.async_set_updated_data(initial_data)
+
     @callback
     def _async_handle_bluetooth_event(
         self,
@@ -53,9 +62,31 @@ class EndersCelsioCoordinator(PassiveBluetoothDataUpdateCoordinator):
     ) -> None:
         """Handle a Bluetooth advertisement event."""
         data = parse_service_info(service_info)
-        if data:
+        if not data:
+            return
+
+        if self.device_data is None:
             self.device_data = data
-            self.async_set_updated_data(data)
+        else:
+            if data.meat_temperature is not None:
+                self.device_data.meat_temperature = data.meat_temperature
+                self.device_data.ambient_temperature = data.ambient_temperature
+                self.device_data.ambient_low = data.ambient_low
+                self.device_data.battery_level = data.battery_level
+                self.device_data.probe_id = data.probe_id
+                self.device_data.raw_meat = data.raw_meat
+                self.device_data.raw_ambient = data.raw_ambient
+                self.device_data.status_byte = data.status_byte
+
+            if data.rssi is not None:
+                self.device_data.rssi = data.rssi
+
+            if data.name:
+                self.device_data.name = data.name
+
+            self.device_data.connected = True
+
+        self.async_set_updated_data(self.device_data)
 
     @property
     def device_info(self) -> DeviceInfo:
